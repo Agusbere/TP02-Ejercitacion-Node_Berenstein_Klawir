d5aed9a35c4dfa7be68537bf91e227f6d3ba563e
Agusbere <48178406@est.ort.edu.ar>
2025-03-28 19:07:01 -0300

    EJ5 hecho

--- src/ej05.js
+++ src/ej05.js
@@ -1,8 +1,14 @@
 let miUrl = null;
 let miObjeto = null;
-miUrl = 'https://www.nike.com.ar/hombre/zapatillas/moda';
-miObjeto = parsearUrl (miUrl);
+miUrl = "https://www.nike.com.ar/air-jordan-4-retro-fq8138-002/p";
+miObjeto = parsearUrl(miUrl);
 console.log(miObjeto);
-function parsearUrl(laURL){
-    laURL = url.parse('https://www.nike.com.ar/hombre/zapatillas/moda')
+function parsearUrl(laURL) {
+    let url = new URL(laURL);
+
+    return {
+        "host": url.host,
+        "pathname": url.pathname,
+        "parametros": url.searchParams
+    };
 }
