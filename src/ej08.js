80e8c2b678a56e4bab3afc0a7fb890747c180701
Android Studio <devandroid@ort>
2025-03-31 12:08:42 -0300

    last commit

--- src/ej08.js
+++ src/ej08.js
@@ -1,10 +1,10 @@
 import {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID} from './modules/omdb-wrapper.js'
 
-let resultado = await OMDBSearchByPage("cars", 1);
+let resultado = await OMDBSearchByPage("Cars", 1);
 console.log("OMDBSearchByPage", resultado);
 
-let resultadoCompleto = await OMDBSearchComplete("cars");
+let resultadoCompleto = await OMDBSearchComplete("Kung Fu Panda");
 console.log("OMDBSearchComplete", resultadoCompleto);
 
-let informacion = await OMDBGetByImdbID("tt0848228");
+let informacion = await OMDBGetByImdbID("tt0198781");
 console.log("OMDBGetByImdbID", informacion);
--- src/modules/omdb-wrapper.js
+++ src/modules/omdb-wrapper.js
@@ -1,6 +1,12 @@
 import axios from "axios";
+import https from "https";
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 const APIKEY = "a41637f6";
+
 const OMDBSearchByPage = async (searchText, page = 1) => {
   let returnObject = {
     respuesta: false,
@@ -8,32 +14,114 @@ const OMDBSearchByPage = async (searchText, page = 1) => {
     datos: [],
   };
 
-  // No seas vago, acá hay que hacer el cuerpo de la función!!!
-  
+  try {
+    const response = await axios.get("https://www.omdbapi.com/", {
+      params: {
+        s: searchText,
+        page: page,
+        apikey: APIKEY,
+      },
+      httpsAgent: agent,
+    });
+
+    if (response.data.Response === "True") {
+      returnObject.respuesta = true;
+      returnObject.cantidadTotal = parseInt(response.data.totalResults, 10);
+      returnObject.datos = response.data.Search;
+    } else {
+      returnObject.respuesta = false;
+      returnObject.datos = [];
+    }
+  } catch (error) {
+    console.error("Error en OMDBSearchByPage:", error);
+    returnObject.respuesta = false;
+    returnObject.datos = [];
+  }
+
   return returnObject;
 };
-const OMDBSearchComplete = async (searchText) => {
+
+const OMDBSearchComplete = async (searchText, page = 1, results = 0) => {
   let returnObject = {
     respuesta: false,
     cantidadTotal: 0,
     datos: [],
   };
 
-  // No seas vago, acá hay que hacer el cuerpo de la función!!!
-  
+  try {
+    const firstResponse = await axios.get("https://www.omdbapi.com/", {
+      params: {
+        s: searchText,
+        page: page,
+        apikey: APIKEY,
+      },
+      httpsAgent: agent,
+    });
+
+    if (firstResponse.data.Response === "True") {
+      results = parseInt(firstResponse.data.totalResults, 10);
+      returnObject.respuesta = true;
+      returnObject.cantidadTotal = results;
+      returnObject.datos = firstResponse.data.Search;
+
+      const pages = Math.ceil(results / 10);
+
+      for (let i = page + 1; i <= pages; i++) {
+        const response = await axios.get("https://www.omdbapi.com/", {
+          params: {
+            texto: searchText,
+            page: i,
+            apikey: APIKEY,
+          },
+          httpsAgent: agent,
+        });
+
+        if (response.data.Response === "True") {
+          returnObject.datos = returnObject.datos.concat(response.data.Search);
+        }
+      }
+    } else {
+      returnObject.respuesta = false;
+      returnObject.datos = [];
+    }
+  } catch (error) {
+    console.error("Error en OMDBSearchComplete:", error);
+    returnObject.respuesta = false;
+    returnObject.datos = [];
+  }
+
   return returnObject;
 };
+
 const OMDBGetByImdbID = async (imdbID) => {
   let returnObject = {
     respuesta: false,
-    cantidadTotal: 0,
     datos: {},
   };
 
-  // No seas vago, acá hay que hacer el cuerpo de la función!!!
-  
+  try {
+    const response = await axios.get("https://www.omdbapi.com/", {
+      params: {
+        i: imdbID,
+        apikey: APIKEY,
+      },
+      httpsAgent: agent,
+    });
+
+    if (response.data.Response === "True") {
+      returnObject.respuesta = true;
+      returnObject.datos = response.data;
+    } else {
+      returnObject.respuesta = false;
+      returnObject.datos = {};
+    }
+  } catch (error) {
+    console.error("Error en OMDBGetByImdbID:", error);
+    returnObject.respuesta = false;
+    returnObject.datos = {};
+  }
+
   return returnObject;
 };
 
-// Exporto todo lo que yo quiero exponer del módulo:
 export { OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID };
