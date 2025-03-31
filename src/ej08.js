d16e279528b075feee4079ce3d1249e03a169208
Agusbere <48178406@est.ort.edu.ar>
2025-03-30 22:56:09 -0300

    EJ8 comenzado

---
+++ src/ej08.js
@@ -0,0 +1,10 @@
+import {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID} from './modules/omdb-wrapper.js'
+
+let resultado = await OMDBSearchByPage("cars", 1);
+console.log("OMDBSearchByPage", resultado);
+
+let resultadoCompleto = await OMDBSearchComplete("cars");
+console.log("OMDBSearchComplete", resultadoCompleto);
+
+let informacion = await OMDBGetByImdbID("tt0848228");
+console.log("OMDBGetByImdbID", informacion);
